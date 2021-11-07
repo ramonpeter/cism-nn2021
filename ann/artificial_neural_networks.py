58bc3682a9564c398e9a5b89043d569270829b0c
Ramon Winterhalder <rpw@mailbox.org>
2021-11-07 22:35:27 +0100

    Update ann

---
+++ ann/artificial_neural_networks.py
@@ -0,0 +1,92 @@
+# Artificial Neural Network
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+tf.__version__
+
+# Part 1 - Data Preprocessing
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Encoding categorical data
+# Label Encoding the "Gender" column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Building the ANN
+
+# Sequential API
+ann = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=6, activation='relu'),
+    tf.keras.layers.Dense(units=6, activation='relu'),
+])
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Functional API
+# inputs = tf.keras.Input(shape=(12,))
+# x = tf.keras.layers.Dense(6, activation="relu")(inputs)
+# x = tf.keras.layers.Dense(6, activation="relu")(x)
+# outputs = tf.keras.layers.Dense(1, activation="sigmoid")(x)
+# f_model = tf.keras.Model(inputs=inputs, outputs=outputs, name="functional_model")
+# f_model.summary()
+
+# Part 3 - Training the ANN
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Part 4 - Making the predictions and evaluating the model
+
+# Predicting the result of a single observation
+
+"""
+Exercise:
+Use our ANN model to predict if the customer with the following informations will leave the bank: 
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40 years old
+Tenure: 3 years
+Balance: $ 60000
+Number of Products: 2
+Does this customer have a credit card? Yes
+Is this customer an Active Member: Yes
+Estimated Salary: $ 50000
+So, should we say goodbye to that customer?
+
+
+
+Solution:
+"""
+
+# TODO
