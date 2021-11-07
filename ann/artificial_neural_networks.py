0012c05492ad1a87c9e87c2337e9e20c4b0f4130
Ramon Winterhalder <rpw@mailbox.org>
2021-11-08 00:00:54 +0100

    Add primer and update first example

--- ann/artificial_neural_networks.py
+++ ann/artificial_neural_networks.py
@@ -89,4 +89,10 @@ So, should we say goodbye to that customer?
 Solution:
 """
 
-# TODO
+print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+"""
+Therefore, our ANN model predicts that this customer stays in the bank!
+Important note 1: Notice that the values of the features were all input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting our values into a double pair of square brackets makes the input exactly a 2D array.
+Important note 2: Notice also that the "France" country was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the first row of the matrix of features X, "France" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, because the dummy variables are always created in the first columns.
+"""
